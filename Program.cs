65c9151509e83a6abb0f7729a7f806befb9ffc28
bot4o <boevaleksandar@gmail.com>
2023-12-09 21:32:35 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filepath = @"calculate.txt";
+
+        var results = new List<object>();
+        try
+        {
+            string[] problems = File.ReadAllLines(filepath);
+            Console.WriteLine("Results: ");
+            foreach(var problem in problems)
+            {
+                var result = Convert.ToDouble(new DataTable().Compute(problem, null));
+                results.Add(result);
+                Console.WriteLine($"{problem} = {result}");
+            }
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("File not found!");
+        }
+        Console.WriteLine();
+        double sum = 0;
+        foreach(double result in results)
+        {
+            sum += result;
+        }
+        Console.WriteLine($"Total entries: {results.Count}");
+        Console.WriteLine($"Total amount: {sum}");
+    }
+}
